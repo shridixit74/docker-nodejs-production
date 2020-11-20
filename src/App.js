da413e557288fdf463a806be981e7ab939e62f24
Shrikant Dixit <shridixit74@gmail.com>
2020-11-19 20:27:00 -0800

    [Feature] - New version of docker in nodejs project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1> yoyoyoyoyoyoyoyoy.............................  </h1>
+          Dr. Dre and eminam .....
+          Hello Nigga ............. Try this out ..
+          Don't act like animal .... gangsta rap made me to it .... mothafucka..
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
